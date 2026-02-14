61af0ba6bab9bc2b4b0f158e75de419b4a8d4ade
Justice Rushwaya <102978652+Mafidibo@users.noreply.github.com>
2026-02-14 11:24:11 +0200

    Add files via upload

---
+++ production-analysis/Mining Data.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+df = pd.read_excel('C:/Users/justi/Downloads/durham.xlsx')
+
+
+# In[19]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
+
+# In[12]:
+
+
+df.describe()
+
+
+# In[20]:
+
+
+df.info()
+
+
+# In[21]:
+
+
+df1 = pd.read_excel('C:/Users/justi/Downloads/longitude.xlsx')
+df1.head()
+
+
+# In[ ]:
+
+
+
+
